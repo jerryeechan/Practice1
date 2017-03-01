1c3b66baedea49467ae2953f585e2f4dde0fffaa
jerrypupu111 <pupu1416@yahoo.com.tw>
2017-03-01 11:05:43 +0800

    comments

--- src/ss/labs/pokemon/Main.java
+++ src/ss/labs/pokemon/Main.java
@@ -1,8 +1,47 @@
 package ss.labs.pokemon;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //TODO declare and initiate a Scanner
+
+        //TODO declare and initiate pokedex as an ArrayList to store pokemon data
+
+        //TODO declare and initiate typeCountMap as a HashMap to count the number of each types of pokemon
+
+        System.out.println("Number of pokemon in the pokedex:");
+        //TODO 0: scan the number of the testcase
+
+        //TODO use a loop to read several input data
+        for...
+        {
+            System.out.println("Enter pokemon name:");
+            //TODO 1-1:scan pokemon name and save to a variable
+
+            System.out.println("Enter pokemon type:");
+            //TODO 1-2:scan pokemon type and save to a variable
+
+            System.out.println("Enter pokemon's move name:");
+            //TODO 1-3:scan pokemon move's name
+
+            //TODO 2-1:create a new pokemon data from the above variables
+
+            //TODO 2-2:add the pokemon data into pokedex
+
+            //TODO 4: Tricky part!! get the value from the type counting map , add 1 and put it back (multiple line
+
+
+        }
+
+        //TODO 5 print out the created pokemon information (about 4 lines
+
+        //TODO 6 print out how many type of pokemons in the pokedex
+
     }
 }
